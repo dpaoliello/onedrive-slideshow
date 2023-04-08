12b1da61037557a18b05926da1891d1d9df1c437
Daniel Paoliello <daniel@meta-sys.info>
2023-04-07 20:26:32 -0700

    Initial version

---
+++ src/auth.rs
@@ -0,0 +1,140 @@
+use crate::http::Client;
+use anyhow::{bail, Context, Result};
+use reqwest::StatusCode;
+use serde::Deserialize;
+use std::time::Duration;
+use tokio::sync::mpsc::Sender;
+
+const CLIENT_ID: &str = "9a021cf1-0d67-456b-b821-c1dff53de0e7";
+const SCOPE: &str = "offline_access files.read";
+
+const DEVICE_CODE_URL: &str = "https://login.microsoftonline.com/consumers/oauth2/v2.0/devicecode";
+const TOKEN_URL: &str = "https://login.microsoftonline.com/consumers/oauth2/v2.0/token";
+
+pub struct Authenticator {
+    client: Client,
+    access_token: Option<String>,
+    refresh_token: Option<String>,
+    on_has_code: Sender<(String, String)>,
+}
+
+#[derive(Deserialize)]
+struct DeviceAuthResponse {
+    device_code: String,
+    user_code: String,
+    verification_uri: String,
+    interval: i32,
+}
+
+#[derive(Deserialize)]
+struct TokenResponseSuccess {
+    access_token: String,
+    refresh_token: String,
+}
+
+#[derive(Deserialize)]
+struct TokenResponseError {
+    error: String,
+    error_description: String,
+}
+
+#[derive(Deserialize)]
+#[serde(untagged)]
+enum TokenResponse {
+    Success(TokenResponseSuccess),
+    Failure(TokenResponseError),
+}
+
+impl Authenticator {
+    pub fn new(on_has_code: Sender<(String, String)>) -> Self {
+        Self {
+            client: Client::new(),
+            access_token: None,
+            refresh_token: None,
+            on_has_code,
+        }
+    }
+
+    pub fn invalidate_token(&mut self) {
+        self.access_token = None;
+    }
+
+    pub async fn get_token(&mut self) -> Result<&str> {
+        if self.access_token.is_none() {
+            let response = if let Some(refresh_token) = &self.refresh_token {
+                self.client
+                    .post::<TokenResponse>(
+                        TOKEN_URL,
+                        &[
+                            ("client_id", CLIENT_ID),
+                            ("grant_type", "refresh_token"),
+                            ("scope", SCOPE),
+                            ("refresh_token", refresh_token),
+                        ],
+                        None,
+                    )
+                    .await
+                    .with_context(|| "Refresh token")?
+            } else {
+                let device_response = self
+                    .client
+                    .post::<DeviceAuthResponse>(
+                        DEVICE_CODE_URL,
+                        &[("client_id", CLIENT_ID), ("scope", SCOPE)],
+                        None,
+                    )
+                    .await
+                    .with_context(|| "Initial auth request")?;
+
+                self.on_has_code
+                    .send((device_response.verification_uri, device_response.user_code))
+                    .await
+                    .unwrap();
+
+                loop {
+                    let token_response = self
+                        .client
+                        .post::<TokenResponse>(
+                            TOKEN_URL,
+                            &[
+                                ("grant_type", "urn:ietf:params:oauth:grant-type:device_code"),
+                                ("client_id", CLIENT_ID),
+                                ("device_code", &device_response.device_code),
+                            ],
+                            Some(StatusCode::BAD_REQUEST),
+                        )
+                        .await
+                        .with_context(|| "Exchange token")?;
+
+                    if let TokenResponse::Failure(TokenResponseError { error, .. }) =
+                        &token_response
+                    {
+                        if error == "authorization_pending" {
+                            tokio::time::sleep(Duration::from_secs(
+                                device_response.interval as u64,
+                            ))
+                            .await;
+                            continue;
+                        }
+                    }
+
+                    break token_response;
+                }
+            };
+
+            match response {
+                TokenResponse::Failure(TokenResponseError {
+                    error_description, ..
+                }) => {
+                    bail!(error_description);
+                }
+                TokenResponse::Success(response) => {
+                    self.refresh_token = Some(response.refresh_token);
+                    self.access_token = Some(response.access_token);
+                }
+            }
+        }
+
+        Ok(self.access_token.as_ref().unwrap())
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,171 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+mod auth;
+mod http;
+mod image_loader;
+
+use anyhow::Result;
+use auth::Authenticator;
+use eframe::{egui, epaint::Rect};
+use egui_extras::RetainedImage;
+use image_loader::ImageLoader;
+use std::{process, time::Duration};
+use tokio::{
+    sync::mpsc::{channel, error::TryRecvError, Receiver, Sender},
+    task,
+    time::Instant,
+};
+
+const DEFAULT_IMAGE_REFRESH_TIME: Duration = Duration::from_secs(1);
+const IMAGE_LIST_REFRESH_TIME: Duration = Duration::from_secs(60 * 60);
+
+fn main() -> Result<(), eframe::Error> {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {
+            let options = eframe::NativeOptions {
+                fullscreen: true,
+                ..Default::default()
+            };
+            eframe::run_native(
+                "OneDrive Slideshow",
+                options,
+                Box::new(move |cc| {
+                    let (sender, receiver) = channel(8);
+                    task::spawn(image_load_loop(sender, cc.egui_ctx.clone()));
+                    Box::new(Slideshow::new(receiver))
+                }),
+            )
+        })
+}
+
+enum AppState {
+    WaitingForAuth(String, String),
+    LoadingImage,
+    HasImage(RetainedImage),
+}
+
+unsafe impl Send for AppState {}
+unsafe impl Sync for AppState {}
+
+struct Slideshow {
+    current_state: Result<AppState>,
+    incoming_state: Receiver<Result<AppState>>,
+}
+
+impl Slideshow {
+    fn new(image_receiver: Receiver<Result<AppState>>) -> Self {
+        Self {
+            current_state: Ok(AppState::LoadingImage),
+            incoming_state: image_receiver,
+        }
+    }
+}
+
+impl eframe::App for Slideshow {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // If it's been long enough between updates, then start getting another image and switch images.
+        match self.incoming_state.try_recv() {
+            Ok(new_state) => self.current_state = new_state,
+            Err(TryRecvError::Disconnected) => process::exit(1),
+            _ => (),
+        }
+
+        egui::CentralPanel::default().show(ctx, |ui|
+            ui.centered_and_justified(|ui|
+                 match &self.current_state {
+            Ok(AppState::LoadingImage) => {
+                ui.spinner();
+            }
+            Ok(AppState::HasImage(image)) => {
+                image.show_max_size(ui, ui.available_size());
+            }
+            Ok(AppState::WaitingForAuth(auth_url, code)) => {
+                ui.label(format!("Authorize the slideshow to read from your OneDrive by opening {auth_url} in a browser and entering the code {code}"));
+            }
+            Err(err) => {
+                ui.colored_label(ui.visuals().error_fg_color, format!("{err:?}")); // something went wrong
+            }
+        }));
+    }
+}
+
+struct ImageList {
+    images: Vec<String>,
+    interval: Duration,
+    last_updated: Instant,
+}
+
+async fn image_load_loop(ui_sender: Sender<Result<AppState>>, ctx: egui::Context) {
+    let (auth_sender, mut auth_receiver) = channel(8);
+    let captured_ui_sender = ui_sender.clone();
+    let captured_ctx = ctx.clone();
+    let _auth_manager = task::spawn(async move {
+        while let Some((auth_url, code)) = auth_receiver.recv().await {
+            send_update(
+                &captured_ui_sender,
+                &captured_ctx,
+                Ok(AppState::WaitingForAuth(auth_url, code)),
+            )
+            .await;
+        }
+    });
+
+    let mut loader = ImageLoader::new(Authenticator::new(auth_sender));
+    let mut all_images = None;
+    loop {
+        match get_next_image(&mut loader, ctx.screen_rect(), &mut all_images).await {
+            Ok(image) => send_update(&ui_sender, &ctx, Ok(AppState::HasImage(image))).await,
+            Err(err) => {
+                send_update(&ui_sender, &ctx, Err(err.context("Loading image"))).await;
+            }
+        }
+
+        tokio::time::sleep(
+            all_images
+                .as_ref()
+                .map_or(DEFAULT_IMAGE_REFRESH_TIME, |all_images| all_images.interval),
+        )
+        .await;
+    }
+}
+
+async fn get_next_image(
+    loader: &mut ImageLoader,
+    size: Rect,
+    all_images: &mut Option<ImageList>,
+) -> Result<RetainedImage> {
+    // Check for expiry.
+    if all_images.as_ref().map_or(false, |list| {
+        Instant::now().duration_since(list.last_updated) >= IMAGE_LIST_REFRESH_TIME
+    }) {
+        *all_images = None;
+    }
+
+    // Get the new list of images if we don't have one.
+    if all_images.is_none() {
+        let (images, interval) = loader.get_image_list().await?;
+        *all_images = Some(ImageList {
+            images,
+            interval: Duration::from_secs(interval),
+            last_updated: Instant::now(),
+        });
+    }
+
+    loader
+        .load_next(
+            size.height() as u32,
+            size.width() as u32,
+            &all_images.as_ref().unwrap().images,
+        )
+        .await
+}
+
+async fn send_update<T>(sender: &Sender<T>, ctx: &egui::Context, message: T) {
+    if sender.send(message).await.is_err() {
+        process::exit(1);
+    }
+    ctx.request_repaint();
+}
